680061fead36c2c4a27922e6423ea4c011469138
xiaoyang <a356510493@hotmail.com>
2021-08-02 15:06:32 +0800

    edit config

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -3,5 +3,6 @@ module.exports = [
   ['meta', { name: 'keywords', content: '个人博客, alva-yky, 前端博客, 学习, markdown' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes'}],
   ['meta', { name: 'full-screen', content: 'true'}],
-  ['meta', { name: 'x5-fullscreen', content: 'true'}]
+  ['meta', { name: 'x5-fullscreen', content: 'true'}],
+  ['meta', { name: 'x5-page-mode', content: 'app'}]
 ]
