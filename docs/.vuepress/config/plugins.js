dcde774933b491f63178dcd728271449756c2d69
xiaoyang-pc <xiaoyang-pc@xiaoyang-pcdeiMac-Pro.local>
2021-08-15 21:43:08 +0800

    update

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,4 +1,14 @@
 module.exports = [
   'element-ui',
-  'vuepress-plugin-element-tabs'
+  'vuepress-plugin-element-tabs',
+  [
+    'one-click-copy',
+    {
+      // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
+      copyMessage: '复制成功',
+      duration: 1000,
+      showInMobile: false
+    }
+  ]
 ]
